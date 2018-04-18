5f718728a7c53a78eae99df85e54b26013f62199
Anwar Jahan <anwar.jahan19@gmail.com>
2018-04-18 19:46:19 -0400

    Project struct

---
+++ src/main/java/com/anwar/dataflow/App.java
@@ -0,0 +1,13 @@
+package com.anwar.dataflow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
